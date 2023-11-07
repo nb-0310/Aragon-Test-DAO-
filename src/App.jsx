e5cd9e9222bb7b05101ce3de0962cbf7ce65356d
Nirzar Bhatt <nirzarbhatt031093@gmail.com>
2023-11-07 16:52:56 +0530

    Connect Wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,40 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+import React, { useState } from "react";
+import { ethers } from "ethers";
 
 const App = () => {
-  const [provider, setProvider] = useState(null);
+  const [address, setAddress] = useState(null);
 
-  // Initialize the Ethereum provider when the component mounts
+  const connect = async () => {
+    if (window.ethereum) {
+      console.log("detected");
 
-  console.log (window.ethereum)
-  useEffect(() => {
-    async function initializeProvider() {
-      if (window.ethereum) {
-        const web3provider = new ethers.providers.Web3Provider(window.ethereum)
-        setProvider(web3provider);
-
-        // Request user's permission to access their MetaMask account
-        try {
-          await window.ethereum.request({ method: 'eth_requestAccounts' });
-        } catch (error) {
-          console.error('User denied account access');
-        }
-      }
-    }
-
-    initializeProvider();
-  }, []);
-
-  // Function to handle the transaction
-  const handleTransaction = async () => {
-    if (provider) {
       try {
-        const signer = provider.getSigner();
-        const transaction = {
-          to: '0xRecipientAddress',
-          value: ethers.utils.parseEther('0.1'), // 0.1 ETH
-        };
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
 
-        const txResponse = await signer.sendTransaction(transaction);
-        await txResponse.wait();
-        console.log('Transaction confirmed:', txResponse.hash);
+        console.log(accounts);
+        setAddress(accounts[0]);
       } catch (error) {
-        console.error('Error sending transaction:', error);
+        console.log(error);
       }
-    } else {
-      console.error('Ethereum provider is not available');
-    }
+    } else console.log("not detected");
+  };
+
+  const signData = async () => {
+    const signer = provider.getSigner();
+    const msg = "Test Message";
+
+    const sign = await signer.signMessage(msg);
+
+    console.log(sign);
   };
 
   return (
     <div>
-      <h1>MetaMask Web3 Transaction Signer</h1>
-      {provider ? (
-        <button onClick={handleTransaction}>Sign Transaction</button>
-      ) : (
-        <p>MetaMask not detected. Please install MetaMask and connect your wallet.</p>
-      )}
+      <button onClick={connect}>Connect Wallet</button>
+      <button onClick={signData}>Sign Transaction</button>
+      <p>Address: {address}</p>
     </div>
   );
 };
